251ca870f933ef31948d06331e678ff32d9a76fe
ParrySMS <yh@szu.me>
2019-07-17 13:50:40 +0800

    Create randNum.cpp

---
+++ randNum.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int LEN = 20;
+const int KEY_INDEX = -1;
+
+int main() {
+
+	int tmp,i,j,rand_index,key;
+	int *arr = new int[LEN];
+
+	for(i=0; i<LEN; i++) {
+		arr[i] = i;
+	}
+
+	srand((unsigned)time(NULL));
+
+	rand_index = rand()%(LEN+1);
+	key = (KEY_INDEX == -1)? rand_index:KEY_INDEX;
+
+	cout<<"key:"<<key<<endl;
+	cout<<endl;
+
+	for(i = LEN -1; i>0; i--) {
+		cout<<endl;
+		rand_index = rand()%(i+1);
+		//swap
+		tmp = arr[i];
+		arr[i] = arr[rand_index];
+		arr[rand_index] = tmp;
+
+		cout<<"swap:arr["<<i<<"]"<<"--arr["<<rand_index<<"]"<<endl;
+		cout<<"after swap:"<<endl;
+		for(j=0; j<LEN; j++) {
+			if(j==i||j==rand_index) {
+				cout<<" *";
+			}
+			cout<<arr[j]<<" ";
+		}
+		cout<<endl;
+	}
+
+	cout<<endl;
+	cout<<"result:"<<arr[key]<<endl;
+
+	return 0;
+}
